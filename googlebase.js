a54444d5b7493135802c6f64854b5fcd3b7f8687
ysilkov <ysilkov@ukr.net>
2023-07-20 16:53:43 +0300

    update

--- googlebase.js
+++ googlebase.js
@@ -1,3 +1,20 @@
+const scrollButton1 = document.getElementById("scrollButton1");
+const scrollButton2 = document.getElementById("scrollButton2");
+const myForm = document.getElementById("contactForm");
+
+
+const scrollToForm = () => {
+  myForm.scrollIntoView({ behavior: "smooth" });
+};
+
+scrollButton1.addEventListener("click", () => {
+  scrollToForm();
+});
+
+scrollButton2.addEventListener("click", () => {
+  scrollToForm();
+});
+
 const submitButton = document.getElementById("submitButton");
 
 function handleFormSubmit(event) {
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const scrollButton1 = document.getElementById("scrollButton1");
 const scrollButton2 = document.getElementById("scrollButton2");
 const myForm = document.getElementById("contactForm");
-const submitButton = document.getElementById("submitButton")
+const submitButton = document.getElementById("submitButton");
 
 const scrollToForm = () => {
   myForm.scrollIntoView({ behavior: "smooth" });
@@ -15,15 +15,46 @@ scrollButton2.addEventListener("click", () => {
   scrollToForm();
 });
 
-const validateForm = () => {
+function validateForm() {
   const requiredInputs = myForm.querySelectorAll("input[required]");
 
   for (const input of requiredInputs) {
     if (!input.value.trim()) {
-      return;
+      return false;
     }
   }
+  return true;
+}
 
-  myForm.submit();
-};
+function handleFormSubmit(event) {
+  event.preventDefault();
 
+  if (validateForm()) {
+    submitButton.disabled = true;
+
+    const formData = {
+      gender: document.getElementById("genderSelect").value,
+      lookingForGender: document.getElementById("genderSelect2").value,
+      payer: document.getElementById("genderSelect3").value,
+      location: document.getElementById("input4").value,
+      birthdate: document.getElementById("input5").value,
+      email: document.getElementById("input6").value,
+    };
+    axios
+      .post(
+        `https://script.google.com/macros/s/AKfycbwmrqjfFaZRnEICWuwMzFrc4G_5HYHv8tUVNkeJ_6-bynCfwMlnU5EVVRW3kmNe-nCg/exec?p1=${formData.gender}&p2=${formData.lookingForGender}&p3=${formData.payer}&p4=${formData.location}&p5=${formData.birthdate}&p6=${formData.email}`
+      )
+      .then((response) => {
+        console.log("Data saved to Google Sheets!");
+        document.getElementById("contactForm").reset();
+      })
+      .catch((error) => {
+        console.error("Error saving data:", error);
+      })
+      .finally(() => {
+        submitButton.disabled = false;
+      });
+  }
+}
+
+submitButton.addEventListener("click", handleFormSubmit);
