747dff1fe7ace3911de0cc13c03cbb489c66804f
ysilkov <ysilkov@ukr.net>
2023-07-20 08:38:51 +0300

    update

--- googlebase.js
+++ googlebase.js
@@ -1,45 +1,27 @@
-const spreadsheetId = '1mvjzroCKQEKnzbUPpU9KGqlre_rBw6Qom6Uf-AkLwn8';
-const submitButton = document.getElementById('submitButton');
-
-var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('track');
-
-function doPost(e) {
-  var params = JSON.parse(e.postData.contents);
-  sheet.appendRow([new Date(), params.page]);
-  return ContentService.createTextOutput(JSON.stringify({ 'result': 'success', 'row': params }));
-}
+const submitButton = document.getElementById("submitButton");
 
 function handleFormSubmit(event) {
   event.preventDefault();
 
   const formData = {
-    gender: document.getElementById('genderSelect').value,
-    lookingForGender: document.getElementById('genderSelect2').value,
-    payer: document.getElementById('genderSelect3').value,
-    location: document.getElementById('input4').value,
-    birthdate: document.getElementById('input5').value,
-    email: document.getElementById('input6').value
+    gender: document.getElementById("genderSelect").value,
+    lookingForGender: document.getElementById("genderSelect2").value,
+    payer: document.getElementById("genderSelect3").value,
+    location: document.getElementById("input4").value,
+    birthdate: document.getElementById("input5").value,
+    email: document.getElementById("input6").value,
   };
-
-  console.log(formData);
-
-  const apiKey = 'AIzaSyBpLN2x8fpko_DSmkrrorzppsjKn-fRl_4';
-
-  axios.post(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/Sheet1:append?valueInputOption=RAW`, {
-    range: 'Sheet1',
-    majorDimension: 'ROWS',
-    values: [Object.values(formData)],
-  }, {
-    headers: {
-      'Authorization': `Bearer ${apiKey}`,
-      'Content-Type': 'application/json',
-    },
-  })
-  .then((response) => {
-    console.log('Data saved to Google Sheets!');
-    document.getElementById('contactForm').reset();
-  })
-  .catch((error) => {
-    console.error('Error saving data:', error);
-  });
+  axios
+    .post(
+      `https://script.google.com/macros/s/AKfycbwmrqjfFaZRnEICWuwMzFrc4G_5HYHv8tUVNkeJ_6-bynCfwMlnU5EVVRW3kmNe-nCg/exec?p1=${formData.gender}&p2=${formData.lookingForGender}&p3=${formData.payer}&p4=${formData.location}&p5=${formData.birthdate}&p6=${formData.email}`
+    )
+    .then((response) => {
+      console.log("Data saved to Google Sheets!");
+      document.getElementById("contactForm").reset();
+    })
+    .catch((error) => {
+      console.error("Error saving data:", error);
+    });
 }
+
+submitButton.addEventListener("click", handleFormSubmit);
--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-const scrollButton1 = document.getElementById('scrollButton1');
-const scrollButton2 = document.getElementById('scrollButton2');
-const myForm = document.getElementById('contactForm');
+const scrollButton1 = document.getElementById("scrollButton1");
+const scrollButton2 = document.getElementById("scrollButton2");
+const myForm = document.getElementById("contactForm");
 
 const scrollToForm = () => {
-  myForm.scrollIntoView({ behavior: 'smooth' });
+  myForm.scrollIntoView({ behavior: "smooth" });
 };
 
-scrollButton1.addEventListener('click', scrollToForm);
-scrollButton2.addEventListener('click', scrollToForm);
-
-
-
-
-
+scrollButton1.addEventListener("click", scrollToForm);
+scrollButton2.addEventListener("click", scrollToForm);
